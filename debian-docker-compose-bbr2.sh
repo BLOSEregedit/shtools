5b20ce2fd423c773c8bd9de88975e3681ca34c29
BLOSEregedit <996666588@qq.com>
2023-10-25 10:31:51 +0800

    second commit

--- debian-docker-compose-bbr2.sh
+++ debian-docker-compose-bbr2.sh
@@ -38,9 +38,17 @@ touch /root/docker-compose.yml
 docker --version
 docker-compose --version
 
+# Print an empty line
+echo
+echo
 
 # Print a prompt message
 echo "现在执行 BBR2 开启，之后会重启服务器"
+# Print an empty line
+echo
+echo
+
 
 # BBR2
 wget --no-check-certificate -q -O bbr2.sh "https://github.com/yeyingorg/bbr2.sh/raw/master/bbr2.sh" && chmod +x bbr2.sh && bash bbr2.sh auto
+
