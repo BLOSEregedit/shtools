bce1d8eed7d974a3211dc65dff78fe419c70773a
BLOSEregedit <996666588@qq.com>
2023-10-26 21:23:51 +0800

    add akamai-cli-edgeworkers

---
+++ curl-nodejs-akamai-edgeworkers-cli.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+echo
+echo "********   00 服务器初始化   ********"
+echo
+# Update packages
+apt update -y
+apt upgrade -y
+apt install expect -y
+
+echo
+echo "********   01 install curl dig   ********"
+echo
+
+# Install necessary dependencies
+apt install curl apt-transport-https ca-certificates software-properties-common gnupg -y
+
+# Install dig
+apt install dnsutils -y
+
+# Add Docker's official GPG key
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+mkdir -p /etc/apt/keyrings
+
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+
+
+echo
+echo "********   02 安装 nodejs   ********"
+echo
+
+NODE_MAJOR=16
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
+
+apt update -y
+apt install nodejs -y
+
+echo
+echo "********   03 安装 Akamai-cli   ********"
+echo
+
+wget https://github.com/akamai/cli/releases/download/v1.5.5/akamai-v1.5.5-linux386 && chmod +x akamai-v1.5.5-linux386 && ./akamai-v1.5.5-linux386
+
+akamai update
+
+akamai install edgeworkers
+
+
+
+
+echo
+echo
+echo "********   安装完成   ********"
+# Verify
+echo
+echo
+curl --version
+echo
+dig -v
---
+++ wget.md
@@ -0,0 +1,22 @@
+
+## curl、dig、bbr2
+wget https://raw.githubusercontent.com/BLOSEregedit/shtools/main/debian-curl-dig-bbr2.sh && chmod +x debian-curl-dig-bbr2.sh && ./debian-curl-dig-bbr2.sh
+
+
+## curl、dig
+wget https://raw.githubusercontent.com/BLOSEregedit/shtools/main/debian-curl-dig.sh && chmod +x debian-curl-dig.sh && ./debian-curl-dig.sh
+
+
+
+## curl、dig、bbr2、docker、docker-compose
+wget https://raw.githubusercontent.com/BLOSEregedit/shtools/main/debian-docker-compose-bbr2.sh && chmod +x debian-docker-compose-bbr2.sh && ./debian-docker-compose-bbr2.sh
+
+
+## curl、dig、docker、docker-compose
+wget https://raw.githubusercontent.com/BLOSEregedit/shtools/main/debian-docker-compose.sh && chmod +x debian-docker-compose.sh && ./debian-docker-compose.sh
+
+## BBR2
+wget --no-check-certificate -q -O bbr2.sh "https://github.com/yeyingorg/bbr2.sh/raw/master/bbr2.sh" && chmod +x bbr2.sh && bash bbr2.sh auto
+
+
+https://github.com/akamai/cli/releases/download/v1.5.5/akamai-v1.5.5-linux386
