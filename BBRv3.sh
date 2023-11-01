a94fabc5d0ee958b9528144bcc1a9e6bd0f63b34
BLOSEregedit <996666588@qq.com>
2023-11-01 17:16:17 +0800

    add bbr3

---
+++ BBRv3.sh
@@ -0,0 +1,27 @@
+# 初始化
+apt update -y && apt install -y wget gnupg
+
+# 注册 PGP 密钥
+wget -qO - https://dl.xanmod.org/archive.key | gpg --dearmor -o /usr/share/keyrings/xanmod-archive-keyring.gpg --yes
+
+
+# 添加存储库
+echo 'deb [signed-by=/usr/share/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' | tee /etc/apt/sources.list.d/xanmod-release.list
+
+
+# 安装
+apt update -y && apt install -y linux-xanmod-x64v4
+
+# 开启 BBR3
+cat > /etc/sysctl.conf << EOF
+net.core.default_qdisc=fq_pie
+net.ipv4.tcp_congestion_control=bbr
+EOF
+
+
+# 加载配置
+sysctl -p
+
+
+# 重启
+reboot
