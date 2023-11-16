86cfc2bbf7673d078858f187e18c6ec58024c007
BLOSEregedit <996666588@qq.com>
2023-11-16 15:03:51 +0800

    update mongo

--- basic/mongoDB.sh
+++ basic/mongoDB.sh
@@ -5,8 +5,9 @@ echo
 echo "01 创建文件夹  -->   /root/mongo/ "
 # Create mongoDB
 mkdir -p /root/mongo /root/mongo/data /root/mongo/log /root/mongo/config
-
+echo
 echo "02 写入配置文件  -->  /root/docker-compose.yml "
+echo
 # Write file content
 cat <<EOF > /root/docker-compose.yml
 ---
@@ -27,12 +28,18 @@ services:
 EOF
 
 # start mongoDB
-echo "********     安装完成  ( ´∀｀)つt[ ]    ********"
+echo
+echo "********    拉取镜像并启动    ********"
 echo
 docker compose up -d
 echo
+echo "********    ( ´∀｀)つt[ okk～ ]    ********"
+echo
 echo "账号：root"
 echo "密码：6Wr^Zivyc%eKZ!bxi7QkP"
+echo
+echo
+echo "container_name：mongo"
 echo "端口：8080"
 echo
 
