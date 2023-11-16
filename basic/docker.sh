cc3c6fd9b9f558ae43d03e58f1b02f6d1e3590da
BLOSEregedit <996666588@qq.com>
2023-11-16 14:41:35 +0800

    update docker install

--- basic/docker.sh
+++ basic/docker.sh
@@ -21,19 +21,20 @@ sudo chmod a+r /etc/apt/keyrings/docker.gpg
 echo
 echo "03 添加资源 "
 echo
+
 # Add the repository to Apt sources:
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
 
 
 
 echo
 echo "04 安装 latest 版本 "
 echo
-sudo apt-get update
-
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
